096408c9ae55fd3a83f479705ed7df0f3892ebcb
Michael Rosanelli <michaelrosanelli105@gmail.com>
2024-11-07 14:15:38 -0500

    added vader analysis

--- nlptest.py
+++ nlptest.py
@@ -1,5 +1,7 @@
+import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from pandas import *
 import spacy
 from pandas import *
@@ -10,12 +12,16 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.sparse import hstack
+
+# nltk.download('vader_lexicon')
 
 nlp = spacy.load("en_core_web_sm")
 
 stop_words = set(stopwords.words('english'))  # Create a set of English stopwords for filtering
+
+vader = SentimentIntensityAnalyzer()
 
 data = read_csv('chris-preprocessed-data.csv', encoding='latin-1')
 
@@ -53,17 +59,19 @@ negative_words = [
     "unfortunate", "disappointed"
 ]
 
-stemmed_positive_words = [stem(word) for word in positive_words]
-stemmed_negative_words = [stem(word) for word in negative_words]
+doc = nlp(" ".join(positive_words))
+lemmatized_positive = [token.lemma_.lower() for token in doc]
+doc = nlp(" ".join(negative_words))
+lemmatized_negative = [token.lemma_.lower() for token in doc]
 
 def getOnlyCount(tokens):
     return len(list((i for i, n in enumerate(tokens) if n == 'only')))
 
 def getPositiveCount(tokens):
-    return sum(1 for word in tokens if word in stemmed_positive_words)
+    return sum(1 for word in tokens if word in lemmatized_positive)
 
 def getNegativeCount(tokens):
-    return sum(1 for word in tokens if word in stemmed_negative_words)
+    return sum(1 for word in tokens if word in lemmatized_negative)
 
 def getOnlySentiment(tokens):
     lines = []
@@ -78,24 +86,31 @@ def getOnlySentiment(tokens):
             continue
     for words in lines:
         for w in words:
-            if w in stemmed_positive_words:
+            if w in lemmatized_positive:
                 if words[-1] == 'not':
                     result += 1
                 else: 
                     result -= 1
-            elif w in stemmed_negative_words:
+            elif w in lemmatized_negative:
                 if words[-1] == 'not':
                     result -= 1
                 else: 
                     result += 1
     return result
 
+def getVaderScore(text):
+    compound = vader.polarity_scores(text)['compound']
+    if compound >= 0:
+        return 0
+    else:
+        return 1
+
 features = []
 
 for r in reviews:
     tokens = word_tokenize(r)
     stemmed = [stem(word.lower()) for word in tokens]
-    features.append([getPositiveCount(stemmed), getNegativeCount(stemmed), getOnlySentiment(stemmed)])
+    features.append([getPositiveCount(stemmed), getNegativeCount(stemmed), getOnlySentiment(stemmed), getVaderScore(r)])
 
 tfidf_vectorizer = TfidfVectorizer()
 tfidf_matrix = tfidf_vectorizer.fit_transform(reviews)
@@ -118,7 +133,7 @@ classifier.fit(x_train, y_train)
 prediction = classifier.predict(x_test)
 print(accuracy_score(y_test, prediction))
 
-classifier = LinearSVC()
+classifier = LinearSVC(dual=True)
 classifier.fit(x_train, y_train)
 prediction = classifier.predict(x_test)
 print(accuracy_score(y_test, prediction))
